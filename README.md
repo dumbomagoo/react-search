d00e3a1697319eef50f69f23a5acc2b8ea14897f
Sarah Young <syoung@MacBook-Pro.local>
2018-07-26 10:35:22 -0400

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# React and Webpack Tutorial
+
+This is based off of the instructions at [How to Create a React app from scratch using Webpack 4](https://medium.freecodecamp.org/part-1-react-app-from-scratch-using-webpack-4-562b1d231e75).  (Can be cloned from [GitHub](https://github.com/pinglinh/simple_webpack_boilerplate))
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  // File to use during generation
+  template: "./src/index.html", 
+  // file name of newly generated file in dist
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    // Use entry prop to point to a file other than ./src/index.js
+    // entry: "./src/app.js",
+    // Specify output example
+    // output: {
+    //   path: path.resolve('dist'),
+    //   filename: 'bundled.js'
+    // },
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: "style-loader"
+      },{
+        loader: "css-loader",
+        options:{
+          modules: true,
+          importLoaders: 1,
+          localIdentName: "[name]_[local]_[hash:base64]",
+          sourceMap: true,
+          minimize: true
+        }
+      }]
+    }]
+  },
+  plugins: [ htmlPlugin ]
+};
